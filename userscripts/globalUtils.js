be3dc9c19a73cb58b9576f02457d91ae9e899fcf
Ravi S. Rāmphal <rramphal@gmail.com>
2020-05-04 21:49:14 -0700

    formatting

--- userscripts/globalUtils.js
+++ userscripts/globalUtils.js
@@ -68,10 +68,8 @@
     let month = '' + (date.getMonth() + 1);
     let day   = '' + date.getDate();
 
-    if (month.length < 2) 
-      month = '0' + month;
-    if (day.length < 2) 
-      day = '0' + day;
+    if (month.length < 2) { month = '0' + month }
+    if (day.length < 2) { day = '0' + day }
 
     return [year, month, day].join('.');
   }
